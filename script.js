2d3c7af4cf476e8c8445319eb58ee2fb15ca6ba5
Tinknight <tinknight7@gmail.com>
2026-05-09 11:48:10 +0630

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
+const buttons = document.querySelectorAll('.icon-button');
+
+buttons.forEach((button) => {
+  button.addEventListener('click', () => {
+    // Quick click animation for a friendly feel
+    button.animate([
+      { transform: 'scale(1)' },
+      { transform: 'scale(0.96)' },
+      { transform: 'scale(1)' }
+    ], {
+      duration: 150,
+      easing: 'ease-out'
+    });
+  });
+});
 
